dded886ec9bee7eb8650ac7c19a684217a342283
piggybank25251 <rakhaakira2204@gmail.com>
2025-01-18 10:42:55 +0700

    post tabel

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Livewire\Posts;
+
+Route::get('posts', Posts::class)->middleware('auth');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
